24e673a91944f7190be9e75cbcd802652f11986a
Michael <58821673+tehmichael@users.noreply.github.com>
2023-10-11 20:47:59 -0500

    + Params and output cleanup
    Added parameter support for tenantId 
    Cleaned up some variable names
    Added Object output
    Refined comments
    is okay now! :D

--- Convert-AADTenantGUIDToO365CompanyDisplayName.ps1
+++ Convert-AADTenantGUIDToO365CompanyDisplayName.ps1
@@ -1,6 +1,11 @@
-# TRY TO CONVERT TENANT ID TO COMPANY NAME
-$tenantId = Read-Host
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [string]$tenantId
+)
 
+# TRY TO CONVERT TENANT ID TO COMPANY NAME
+
+# Lookup tenant region scope
 $webResponse = Invoke-WebRequest -Uri "https://login.microsoftonline.com/$tenantId/v2.0/.well-known/openid-configuration"
 $webResponseHash = $webResponse.Content | ConvertFrom-Json -AsHashtable
 
@@ -8,18 +13,21 @@ $webResponseHash = $webResponse.Content | ConvertFrom-Json -AsHashtable
 # TODO: Support China TLD
 if ( $webResponseHash.tenant_region_scope -eq "USGov" ) { $tld = "us" } else { $tld = "com" }
 
-# Pull company display name attribute from sign in page
+# Pull company sign in page that may contain a valid Company Display Name
 $webResponse2 = Invoke-WebRequest -Uri "https://login.microsoftonline.$tld/$tenantId/oauth2/v2.0/authorize?client_id=foo&response_type=code&scope=read"
 
 # I put on my robe and wizard hat
 $regexPattern = '"sCompanyDisplayName":\"(.+?)\"'
-$matches = [Regex]::Matches($webResponse2, $regexPattern)
+$regexMatch = [Regex]::Matches($webResponse2, $regexPattern)
 
 # Remove crap
-$companyDisplayName = $matches.Value.Replace('"sCompanyDisplayName":"',"")
+$companyDisplayName = $regexMatch.Value.Replace('"sCompanyDisplayName":"',"")
 $companyDisplayName = $companyDisplayName.Replace('"',"")
 
 # ???
+$companyDisplayNameObject = [PSCustomObject]@{
+    "CompanyDisplayName" = $companyDisplayName
+}
 
 # Profit!
-$companyDisplayName
+$companyDisplayNameObject
